ee47edec29e1787e96faa8d41ab1b448e3273de5
Azael López <alopezg24@fpcoruna.afundacion.org>
2025-11-10 17:30:00 +0100

    Tarea #4

--- sprint3android/app/build.gradle.kts
+++ sprint3android/app/build.gradle.kts
@@ -37,6 +37,8 @@ android {
 
 dependencies {
 
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.9.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
